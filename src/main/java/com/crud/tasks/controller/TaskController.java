e1a40beab5d63acca1ba7d919fe79b3ab1796004
bkomorniczak <b.komorniczak@gmail.com>
2020-06-30 09:16:37 +0200

    annotations added

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,9 +1,7 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,17 +15,17 @@ public class TaskController {
         return new ArrayList<>();
     }
     @RequestMapping(method = RequestMethod.GET,value = "getTask")
-    public TaskDto getTask(Long taskId){
+    public TaskDto getTask(@RequestParam Long taskId){
         return new TaskDto( 1L, "test title", "test_content");
     }
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
-    public void deleteTask(Long taskId){}
+    public void deleteTask(@RequestParam Long taskId){}
 
     @RequestMapping(method = RequestMethod.POST, value = "updateTask")
-    public TaskDto updateTask(TaskDto taskDto){
+    public TaskDto updateTask(@RequestBody TaskDto taskDto){
         return new TaskDto(1L, "Edited test title", "Test content");
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createTask")
-    public void createTask(TaskDto taskDto){}
+    public void createTask(@RequestBody TaskDto taskDto){}
 }
