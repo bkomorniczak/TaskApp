906a52726c75d1229925090b452c724fc62cf9bc
bkomorniczak <b.komorniczak@gmail.com>
2020-07-13 16:01:33 +0200

    Another try 1

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -11,7 +11,6 @@ public class TasksApplication //extends SpringBootServletInitializer
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
-
 	/*@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder){
 		return applicationBuilder.sources(TasksApplication.class);
